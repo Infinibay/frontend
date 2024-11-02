da7b781309e0a8ed441c0a019c29c0880aa91feb
Andres Borek <andres.b.dev@gmail.com>
2024-11-02 20:41:27 +0000

    Starting to give stile to newPC section

--- src/components/CreatePC/OSSelector.jsx
+++ src/components/CreatePC/OSSelector.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import WindowsSelectorList from './WindowsSelectorList';
 import LinuxSelectorList from './LinuxSelectorList';
 import OSItem from "@/components/CreatePC/OSItem";
+import OSSwitch from "@/components/CreatePC/OSSwitch";
 
 const OSSelector = ({ onChange }) => {
     const [osOption, setOsOption] = useState('windows');
@@ -12,6 +13,7 @@ const OSSelector = ({ onChange }) => {
     const [vmName, setVmName] = useState("");
     const [username, sertUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleOsChange = (os) => {
         setSelectedOS(os);
@@ -29,8 +31,12 @@ const OSSelector = ({ onChange }) => {
         sertUsername(e.target.value);
     };
 
-    const handlePassword = (e) =>{
+    const handlePassword = (e) => {
         setPassword(e.target.value);
+    };
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
     };
 
     // Clear the licensing field and selected OS when the OS option (Windows/Linux) changes
@@ -43,7 +49,7 @@ const OSSelector = ({ onChange }) => {
     useEffect(() => {
         let response = {
             os: selectedOS,
-            productKey:  licenseIncluded,
+            productKey: licenseIncluded,
             name: vmName,
             username: username,
             password: password
@@ -52,48 +58,79 @@ const OSSelector = ({ onChange }) => {
     }, [osOption, selectedOS, licenseIncluded, vmName, username, password]);
 
     return (
-        <div>
-            <div onChange={(e) => setOsOption(e.target.value)}>
-                <input type="radio" value="windows" name="os" defaultChecked/> Windows
-                <input type="radio" value="linux" name="os"/> Linux
+        <div className="p-4 max-w-4xl mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="order-2 md:order-1">
+                    <div className="mt-4 max-w-lg">
+                        <label htmlFor="vmName" className="block text-sm font-medium text-gray-700">Name</label>
+                        <input id="vmName"
+                            type="text"
+                            value={vmName}
+                            onChange={(e) => handleNameChange(e)}
+                            placeholder="Machine Name"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        />
+                    </div>
+                    <div className="mt-4 max-w-lg">
+                        <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+                        <input id="username"
+                            type="text"
+                            value={username}
+                            onChange={(e) => handleUsername(e)}
+                            placeholder="Username"
+                            maxLength="30"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        />
+                    </div>
+                    <div className="mt-4 max-w-lg relative">
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                        <input id="password"
+                            type={showPassword ? "text" : "password"}
+                            value={password}
+                            onChange={(e => handlePassword(e))}
+                            placeholder="Password"
+                            maxLength="30"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        />
+                        <button
+                            type="button"
+                            onClick={toggleShowPassword}
+                            className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+                            style={{ top: '50%', transform: 'translateY(-10%)' }}
+                        >
+                            {showPassword ? (
+                                <svg className="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                </svg>
+                            ) : (
+                                <svg className="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7a10.05 10.05 0 011.658-3.033m2.197-2.197A9.956 9.956 0 0112 5c4.477 0 8.268 2.943 9.542 7-.33 1.052-.81 2.036-1.416 2.925M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3l18 18" />
+                                </svg>
+                            )}
+                        </button>
+                    </div>
+
+                    {osOption === 'windows' && (
+                        <WindowsSelectorList
+                            onOsChange={handleOsChange}
+                            onLicenseChange={handleLicenseChange}
+                            defaultSelected='win11'
+                        />
+                    )}
+
+                    {osOption === 'linux' && (
+                        <LinuxSelectorList onOsChange={handleOsChange} />
+                    )}
+                </div>
+                <div className="order-1 md:order-2 flex justify-center md:justify-end">
+                    <OSSwitch onChange={(os) => setOsOption(os)} defaultValue={"windows"}></OSSwitch>
+                </div>
             </div>
-            <br/>
-            <h3>Name</h3>
-            <input id="vmName"
-                   type="text"
-                   value={vmName}
-                   onChange={(e) => handleNameChange(e)}
-                   placeholder={"Machine Name"}
-            />
-            <br/>
-            <input id="username"
-                   type="text"
-                   value={username}
-                   onChange={(e) => handleUsername(e)}
-                   placeholder={"Username"}
-            />
-            <input id="password"
-                   type="text"
-                   value={password}
-                   onChange={(e => handlePassword(e))}
-                   placeholder={"Password"}
-            />
-
-            {osOption === 'windows' && (
-                <WindowsSelectorList
-                    onOsChange={handleOsChange}
-                    onLicenseChange={handleLicenseChange}
-                    defaultSelected='win11'
-                />
-            )}
-
-            {osOption === 'linux' && (
-                <LinuxSelectorList onOsChange={handleOsChange}/>
-            )}
         </div>
     );
-}
-;
+};
 
 OSSelector.propTypes = {
     onChange: PropTypes.func,
@@ -101,7 +138,7 @@ OSSelector.propTypes = {
 
 // Default prop values
 OSSelector.defaultProps = {
-    onClick: () => {},
+    onClick: () => { },
 };
 
 export default OSSelector;
