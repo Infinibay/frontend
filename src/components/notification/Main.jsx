ec9864d3a49d75c22215c8d71605e8fe3d0fdf4d
Danilo Borek <danilo.dev@gmail.com>
2024-11-05 19:22:51 +1300

    Initialising beta branch

--- src/components/notification/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Image from "next/image";
-import NotificationCard from "./NotificationCard";
-
-const Main = () => {
-  return <NotificationCard />;
-};
-
-export default Main;
--- src/utils/search-params.js
+++
@@ -1,7 +0,0 @@
-import { useSearchParams } from "next/navigation";
-
-export function Searchparams(string) {
-  const searchparam = useSearchParams();
-  const id = searchparam.get(string);
-  return id;
-}
